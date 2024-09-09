19fb1f9f1e1de854cfbfe53b42452a2fdef4a45c
Oğuzhan SEPETCİ <oguzhansepetci@gmail.com>
2024-09-09 18:07:30 +0300

    OguZzZ

---
+++ example/githubST7920/Core/Inc/st12864h.h
@@ -0,0 +1,58 @@
+/*
+ * st12864H.h
+ *
+ * Created on: Sep 2, 2024
+ * Author: yazilim
+ */
+
+#ifndef ST12864H_H_
+#define ST12864H_H_
+
+#include "stm32f4xx_hal.h"
+
+#define PIN  GPIO_PIN_4// SEÇİLEN CS PİNİ
+#define PORT GPIOA// SEÇİLEN CS PİN PORTU
+
+#define LCD_CLS         0x01
+#define LCD_HOME        0x02
+#define LCD_ADDRINC     0x06
+#define LCD_DISPLAYON   0x0C
+#define LCD_DISPLAYOFF  0x08
+#define LCD_CURSORON    0x0E
+#define LCD_CURSORBLINK 0x0F
+#define LCD_BASIC       0x30
+#define LCD_EXTEND      0x34
+#define LCD_GFXMODE     0x36
+#define LCD_TXTMODE     0x34
+#define LCD_STANDBY     0x01
+#define LCD_SCROLL      0x03
+#define LCD_SCROLLADDR  0x40
+#define LCD_ADDR        0x80
+#define LCD_LINE0       0x80
+#define LCD_LINE1       0x90
+#define LCD_LINE2       0x88
+#define LCD_LINE3       0x98
+
+
+
+
+void ST7920_Clear(void);
+void Delay_Interup_us(uint16_t us);
+void ST7920_Update(void);
+
+
+void ST7920_SendCmd(uint8_t cmd);
+void ST7920_SendData(uint8_t data);
+void ST7920_GraphicMode(int enable);
+void ST7920_init(void);
+void ST7920_DrawBitmap(const unsigned char* graphic);
+void ST7920_SendString(int row, int col, char* string);
+void DrawLine(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1);
+void DrawRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h);
+void DrawFilledRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h);
+void DrawCircle(uint8_t x0, uint8_t y0, uint8_t radius);
+void DrawFilledCircle(int16_t x0, int16_t y0, int16_t r);
+void DrawTriangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t x3, uint16_t y3);
+void DrawFilledTriangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t x3, uint16_t y3);
+
+#endif /* ST12864H_H_ */
---
+++ example/githubST7920/Core/Src/st12864.c
@@ -0,0 +1,473 @@
+/*
+ * st12864.c
+ *
+ * Created on: Sep 2, 2024
+ * Author: yazilim
+ */
+
+#include "st12864h.h"
+
+extern SPI_HandleTypeDef hspi1;
+extern TIM_HandleTypeDef htim1;
+
+
+
+uint8_t startRow, startCol, endRow, endCol;
+uint8_t numRows = 64;
+uint8_t numCols = 128;
+uint8_t Graphic_Check = 0;
+uint8_t lcd_data[3];
+uint8_t image[(128 * 64)/8];
+
+void Delay_Interup_us(uint16_t us)
+{
+
+    __HAL_TIM_SET_COUNTER(&htim1, 0);
+    while (__HAL_TIM_GET_COUNTER(&htim1) < us);
+}
+
+void ST7920_SendCmd(uint8_t cmd)
+{
+	HAL_GPIO_WritePin(PORT, PIN, GPIO_PIN_SET);
+
+    lcd_data[0] = 0xF8;
+    lcd_data[1] = (cmd & 0xF0);
+    lcd_data[2] = ((cmd << 4) & 0xF0);
+
+    HAL_SPI_Transmit(&hspi1, lcd_data, sizeof(lcd_data), HAL_MAX_DELAY);
+    Delay_Interup_us(30);
+    HAL_GPIO_WritePin(PORT, PIN, GPIO_PIN_RESET);
+}
+
+void ST7920_SendData(uint8_t data)
+{
+	HAL_GPIO_WritePin(PORT, PIN, GPIO_PIN_SET);
+
+    lcd_data[0] = 0xFA;
+    lcd_data[1] = (data & 0xF0);
+    lcd_data[2] = ((data << 4) & 0xF0);
+
+    HAL_SPI_Transmit(&hspi1, lcd_data, sizeof(lcd_data), HAL_MAX_DELAY);
+    Delay_Interup_us(30);
+    HAL_GPIO_WritePin(PORT, PIN, GPIO_PIN_RESET);
+}
+
+void ST7920_GraphicMode(int enable)
+{
+    if (enable == 1)
+    {
+        ST7920_SendCmd(0x30);
+        HAL_Delay(1);
+        ST7920_SendCmd(0x34);
+        HAL_Delay(1);
+        ST7920_SendCmd(0x36);
+        HAL_Delay(1);
+        Graphic_Check = 1;
+    }
+    else if (enable == 0)
+    {
+        ST7920_SendCmd(0x30);
+        HAL_Delay(1);
+        Graphic_Check = 0;
+    }
+}
+
+void ST7920_init(void)
+{
+	  HAL_TIM_Base_Start(&htim1);  // Timer'ı başlat
+
+    ST7920_SendCmd(0x30);
+    HAL_GPIO_WritePin(PORT, PIN, GPIO_PIN_RESET);
+    HAL_Delay(10);
+    HAL_GPIO_WritePin(PORT, PIN, GPIO_PIN_SET);
+    Delay_Interup_us(110);
+    ST7920_SendCmd(0x30);
+    Delay_Interup_us(40);
+    ST7920_SendCmd(0x08);
+    Delay_Interup_us(110);
+    ST7920_SendCmd(0x01);
+    HAL_Delay(12);
+    ST7920_SendCmd(0x06);
+    HAL_Delay(1);
+    ST7920_SendCmd(0x0C);
+    HAL_Delay(1);
+    ST7920_SendCmd(0x02);
+    HAL_Delay(1);
+}
+
+void ST7920_DrawBitmap(const unsigned char* graphic)
+{
+	uint8_t x, y;
+	for(y = 0; y < 64; y++)
+	{
+		if(y < 32)
+		{
+			for(x = 0; x < 8; x++)
+			{
+				ST7920_SendCmd(0x80 | y);
+				ST7920_SendCmd(0x80 | x);
+				ST7920_SendData(graphic[2*x + 16*y]);
+				ST7920_SendData(graphic[2*x+1 + 16*y]);
+			}
+		}
+		else
+		{
+			for(x = 0; x < 8; x++)
+			{
+				ST7920_SendCmd(0x80 | (y-32));
+				ST7920_SendCmd(0x88 | x);
+				ST7920_SendData(graphic[2*x + 16*y]);
+				ST7920_SendData(graphic[2*x+1 + 16*y]);
+			}
+		}
+
+	}
+}
+
+void ST7920_SendString(int row, int col, char* string)
+{
+    switch (row)
+    {
+        case 0:
+            col |= 0x80;
+            break;
+        case 1:
+            col |= 0x90;
+            break;
+        case 2:
+            col |= 0x88;
+            break;
+        case 3:
+            col |= 0x98;
+            break;
+        default:
+            col |= 0x80;
+            break;
+    }
+
+    ST7920_SendCmd(col);
+
+    while (*string)
+    	{
+    		ST7920_SendData(*string++);
+    	}
+}
+
+
+void ST7920_Clear()
+{
+	if (Graphic_Check == 1)
+	{
+		uint8_t x, y;
+		for(y = 0; y < 64; y++)
+		{
+			if(y < 32)
+			{
+				ST7920_SendCmd(0x80 | y);
+				ST7920_SendCmd(0x80);
+			}
+			else
+			{
+				ST7920_SendCmd(0x80 | (y-32));
+				ST7920_SendCmd(0x88);
+			}
+			for(x = 0; x < 8; x++)
+			{
+				ST7920_SendData(0);
+				ST7920_SendData(0);
+			}
+		}
+	}
+
+	else
+	{
+		ST7920_SendCmd(0x01);
+		HAL_Delay(2);
+	}
+}
+
+
+void ST7920_Update(void)
+{
+   ST7920_DrawBitmap(image);
+}
+
+
+static void SetPixel(uint8_t x, uint8_t y)
+{
+  if (y < numRows && x < numCols)
+  {
+    uint8_t *p = image + ((y * (numCols/8)) + (x/8));
+    *p |= 0x80u >> (x%8);
+
+    *image = *p;
+
+    // Change the dirty rectangle to account for a pixel being dirty (we assume it was changed)
+    if (startRow > y) { startRow = y; }
+    if (endRow <= y)  { endRow = y + 1; }
+    if (startCol > x) { startCol = x; }
+    if (endCol <= x)  { endCol = x + 1; }
+
+
+  }
+
+}
+
+/* draw a line
+ * start point (X0, Y0)
+ * end point (X1, Y1)
+ */
+void DrawLine(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1)
+{
+  int dx = (x1 >= x0) ? x1 - x0 : x0 - x1;
+  int dy = (y1 >= y0) ? y1 - y0 : y0 - y1;
+  int sx = (x0 < x1) ? 1 : -1;
+  int sy = (y0 < y1) ? 1 : -1;
+  int err = dx - dy;
+
+  for (;;)
+  {
+    SetPixel(x0, y0);
+    if (x0 == x1 && y0 == y1) break;
+    int e2 = err + err;
+    if (e2 > -dy)
+    {
+      err -= dy;
+      x0 += sx;
+    }
+    if (e2 < dx)
+    {
+      err += dx;
+      y0 += sy;
+    }
+  }
+}
+
+
+
+
+/* Draw rectangle
+ * start point (x,y)
+ * w -> width
+ * h -> height
+ */
+void DrawRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h)
+{
+	/* Check input parameters */
+	if (
+		x >= numCols ||
+		y >= numRows
+	) {
+		/* Return error */
+		return;
+	}
+
+	/* Check width and height */
+	if ((x + w) >= numCols) {
+		w = numCols - x;
+	}
+	if ((y + h) >= numRows) {
+		h = numRows - y;
+	}
+
+	/* Draw 4 lines */
+	DrawLine(x, y, x + w, y);         /* Top line */
+	DrawLine(x, y + h, x + w, y + h); /* Bottom line */
+	DrawLine(x, y, x, y + h);         /* Left line */
+	DrawLine(x + w, y, x + w, y + h); /* Right line */
+}
+
+
+
+
+/* Draw filled rectangle
+ * Start point (x,y)
+ * w -> width
+ * h -> height
+ */
+void DrawFilledRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h)
+{
+	uint8_t i;
+
+	/* Check input parameters */
+	if (
+		x >= numCols ||
+		y >= numRows
+	) {
+		/* Return error */
+		return;
+	}
+
+	/* Check width and height */
+	if ((x + w) >= numCols) {
+		w = numCols - x;
+	}
+	if ((y + h) >= numRows) {
+		h = numRows - y;
+	}
+
+	/* Draw lines */
+	for (i = 0; i <= h; i++) {
+		/* Draw lines */
+		DrawLine(x, y + i, x + w, y + i);
+	}
+}
+
+
+
+
+/* draw circle
+ * centre (x0,y0)
+ * radius = radius
+ */
+void DrawCircle(uint8_t x0, uint8_t y0, uint8_t radius)
+{
+  int f = 1 - (int)radius;
+  int ddF_x = 1;
+
+  int ddF_y = -2 * (int)radius;
+  int x = 0;
+
+  SetPixel(x0, y0 + radius);
+  SetPixel(x0, y0 - radius);
+  SetPixel(x0 + radius, y0);
+  SetPixel(x0 - radius, y0);
+
+  int y = radius;
+  while(x < y)
+  {
+    if(f >= 0)
+    {
+      y--;
+      ddF_y += 2;
+      f += ddF_y;
+    }
+    x++;
+    ddF_x += 2;
+    f += ddF_x;
+    SetPixel(x0 + x, y0 + y);
+    SetPixel(x0 - x, y0 + y);
+    SetPixel(x0 + x, y0 - y);
+    SetPixel(x0 - x, y0 - y);
+    SetPixel(x0 + y, y0 + x);
+    SetPixel(x0 - y, y0 + x);
+    SetPixel(x0 + y, y0 - x);
+    SetPixel(x0 - y, y0 - x);
+  }
+}
+
+
+// Draw Filled Circle
+
+void DrawFilledCircle(int16_t x0, int16_t y0, int16_t r)
+{
+	int16_t f = 1 - r;
+	int16_t ddF_x = 1;
+	int16_t ddF_y = -2 * r;
+	int16_t x = 0;
+	int16_t y = r;
+
+    SetPixel(x0, y0 + r);
+    SetPixel(x0, y0 - r);
+    SetPixel(x0 + r, y0);
+    SetPixel(x0 - r, y0);
+    DrawLine(x0 - r, y0, x0 + r, y0);
+
+    while (x < y) {
+        if (f >= 0) {
+            y--;
+            ddF_y += 2;
+            f += ddF_y;
+        }
+        x++;
+        ddF_x += 2;
+        f += ddF_x;
+
+        DrawLine(x0 - x, y0 + y, x0 + x, y0 + y);
+        DrawLine(x0 + x, y0 - y, x0 - x, y0 - y);
+
+        DrawLine(x0 + y, y0 + x, x0 - y, y0 + x);
+        DrawLine(x0 + y, y0 - x, x0 - y, y0 - x);
+    }
+}
+
+
+
+// Draw Traingle with coordimates (x1, y1), (x2, y2), (x3, y3)
+void DrawTriangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t x3, uint16_t y3)
+{
+	/* Draw lines */
+	DrawLine(x1, y1, x2, y2);
+	DrawLine(x2, y2, x3, y3);
+	DrawLine(x3, y3, x1, y1);
+}
+
+
+
+// Draw Filled Traingle with coordimates (x1, y1), (x2, y2), (x3, y3)
+void DrawFilledTriangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t x3, uint16_t y3)
+{
+	int16_t deltax = 0, deltay = 0, x = 0, y = 0, xinc1 = 0, xinc2 = 0,
+	yinc1 = 0, yinc2 = 0, den = 0, num = 0, numadd = 0, numpixels = 0,
+	curpixel = 0;
+
+#define ABS(x)   ((x) > 0 ? (x) : -(x))
+
+	deltax = ABS(x2 - x1);
+	deltay = ABS(y2 - y1);
+	x = x1;
+	y = y1;
+
+	if (x2 >= x1) {
+		xinc1 = 1;
+		xinc2 = 1;
+	} else {
+		xinc1 = -1;
+		xinc2 = -1;
+	}
+
+	if (y2 >= y1) {
+		yinc1 = 1;
+		yinc2 = 1;
+	} else {
+		yinc1 = -1;
+		yinc2 = -1;
+	}
+
+	if (deltax >= deltay){
+		xinc1 = 0;
+		yinc2 = 0;
+		den = deltax;
+		num = deltax / 2;
+		numadd = deltay;
+		numpixels = deltax;
+	} else {
+		xinc2 = 0;
+		yinc1 = 0;
+		den = deltay;
+		num = deltay / 2;
+		numadd = deltax;
+		numpixels = deltay;
+	}
+
+	for (curpixel = 0; curpixel <= numpixels; curpixel++)
+	{
+		DrawLine(x, y, x3, y3);
+
+		num += numadd;
+		if (num >= den) {
+			num -= den;
+			x += xinc1;
+			y += yinc1;
+		}
+		x += xinc2;
+		y += yinc2;
+	}
+}
+
+
+
+
+
+
+
